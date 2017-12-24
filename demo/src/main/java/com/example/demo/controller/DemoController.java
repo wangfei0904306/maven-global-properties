2c67585e5e03266108cf54b58ecc3fd67e83332f
wangfei <wangfei@iciyun.com>
2017-12-24 15:26:12 +0800

    create

---
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @Value("${demo.content}")
+    String content;
+
+    @GetMapping("/get")
+    public String get(){
+        return content;
+    }
+
+}
